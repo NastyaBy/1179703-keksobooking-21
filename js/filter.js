ad33675a3c3bb660f512204b047ff8ff32a526be
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2020-11-09 15:53:16 +0300

    Добавление переменных фильтра

---
+++ js/filter.js
@@ -0,0 +1,24 @@
+'use strict';
+(() => {
+
+  // 7.1
+}
+
+const housingTypeFilter = mapFilters.querySelector(`#housing-type`);
+const housingPriceFilter = mapFilters.querySelector(`#housing-price`);
+const housingRoomsFilter = mapFilters.querySelector(`#housing-rooms`);
+const housingGuestsFilter = mapFilters.querySelector(`#housing-guests`);
+const housingFeaturesList = mapFilters.querySelectorAll(`.map__checkbox`);
+
+// const filterPinsByType = (value) => {
+//   получение данных
+//   if (выбор нужных)
+//   {
+//      оставить
+//   }
+//    else {
+//       удаление ненужних
+//   }
+// }
+
+})();
--- js/map.js
+++ js/map.js
@@ -89,22 +89,6 @@
     window.server.load(renderPins, onLoadEroor);
   };
 
-// 7.1
-
-  const housingTypeFilter = mapFilters.querySelector(`#housing-type`);
-  const housingPriceFilter = mapFilters.querySelector(`#housing-price`);
-  const housingRoomsFilter = mapFilters.querySelector(`#housing-rooms`);
-  const housingGuestsFilter = mapFilters.querySelector(`#housing-guests`);
-  const housingFeaturesList = mapFilters.querySelectorAll(`.map__checkbox`);
-
-
-
-
-
-
-
-
-
   const reset = () => {
     isPageActive = false;
     changeFormState();
@@ -122,6 +106,7 @@
     activate,
     getIsPageActive,
     mapPins,
+    mapFilters,
     reset,
     initialize
   };
